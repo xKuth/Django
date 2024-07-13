b5f0a911397523f2e35599c897fc98239740e943
xKuth <luisyeager20@gmail.com>
2024-07-13 20:56:13 -0300

    atualizando django

--- Primeiro projeto/lerning_log/learning_logs/models.py
+++ Primeiro projeto/lerning_log/learning_logs/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class topic(models.Model):
+class Topic(models.Model):
     """Um asssunto sobre qual o úsuario esta aprendendo"""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
@@ -9,4 +9,15 @@ class topic(models.Model):
         """Devolve uma representação em string do modelo."""
         return self.text
 
+class Entry(models.Model):
+    """Algo especififco aprendido sobre um assunto"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Delvolve uma representação em string do modelo"""
+        return self.text[:50] + '...'
