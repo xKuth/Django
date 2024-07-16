5562c620528b7bb80a8354f4d3108a7fc966bab2
xKuth <luisyeager20@gmail.com>
2024-07-15 22:10:24 -0300

    Att do site Django

--- Primeiro projeto/learning_log/learning_logs/views.py
+++ Primeiro projeto/learning_log/learning_logs/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from . models import Topic
 
 # Create your views here.
 
 def index(request):
     """Pagina principal do learning_log"""
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """Mostra todos os assuntos"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
