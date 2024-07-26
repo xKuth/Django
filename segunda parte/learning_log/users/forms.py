4567f76e29cd620fbcf13289260acc960baf8e64
xKuth <luisyeager20@gmail.com>
2024-07-26 20:27:04 -0300

    att do login

--- segunda parte/learning_log/users/forms.py
+++ segunda parte/learning_log/users/forms.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django import forms
+from django.core.exceptions import ValidationError
 
 
 # class LoginForm(forms.ModelForm):
@@ -11,3 +12,22 @@ from django import forms
 class LoginForm(forms.Form):
     login = forms.CharField(max_length=30)
     senha = forms.CharField(max_length=30, widget=(forms.PasswordInput))
+
+
+    # Seu eu precisar pegar dois campos ao memo tempo ou mais 
+    # Ultiliza a função clean da seguinte maneira
+    """Def clean(self)
+            cleanned_data = super().clean
+            nome = clenned_data.get('login')
+            senha = cleanned_data.get('senha')
+            
+            Validação
+        """
+    
+
+    def clean_login(self):
+        nome = self.cleaned_data['login']
+        if not(nome.isalnum()):
+            raise ValidationError('O nome de usuário não pode ter caractere especial')
+        return nome
+
--- segunda parte/learning_log/users/views.py
+++ segunda parte/learning_log/users/views.py
@@ -7,18 +7,23 @@ from django.contrib.auth import login as auh_login
 
 
 def login(request):
+    error = False
     if request.method != 'POST':
         form = LoginForm()
     else:
         form = LoginForm(data=request.POST)
         if form.is_valid():
-            username = request.POST.get('username')
-            password = request.POST.get('password')
+            # username = request.POST.get('username')
+            # password = request.POST.get('password')
+            username = request.POST.get('login')
+            password = request.POST.get('senha')
             user = authenticate(username=username, password=password)
             if user:
                 auh_login(request, user)
                 return HttpResponseRedirect(reverse('index'))
-    context = {'form': form}
+            else:
+                error = True
+    context = {'form': form, 'error': error}
     return render(request, 'users/login.html', context)
 
 
